bc4ef293da215399cf1d3b6fa9ccd8e02c32b3c7
Eli Plaut <eli@sentinelone.com>
2016-06-19 08:39:42 +0300

    Tests: move run_server and ButlerTest to fixtures.py

---
+++ tests/fixtures.py
@@ -0,0 +1,35 @@
+import time
+import slash
+import requests
+import threading
+
+from butler import Butler
+
+class ButlerTest(Butler):
+    def get_test_get(self):
+        return 'test get'
+
+    def post_test_post(self):
+        return 'test post'
+        
+    def put_test_put(self):
+        return 'test put'
+        
+    def delete_test_delete(self):
+        return 'test delete'
+
+
+@slash.fixture
+def run_server():
+    butler = ButlerTest()
+    t = threading.Thread(target=butler.run, kwargs={'host': 'localhost', 'port': '8888'})
+    t.daemon = True
+    t.start()
+    time.sleep(1)
+
+    def stop_server():
+        requests.get('http://localhost:8888/stop')
+        time.sleep(1)
+
+    slash.add_cleanup(stop_server)
+
--- tests/test_methods.py
+++ tests/test_methods.py
@@ -1,36 +1,8 @@
-import time
 import slash
 import requests
-import threading
 
-from butler import Butler
+from .fixtures import run_server
 
-class ButlerTest(Butler):
-    def get_test_get(self):
-        return 'test get'
-
-    def post_test_post(self):
-        return 'test post'
-        
-    def put_test_put(self):
-        return 'test put'
-        
-    def delete_test_delete(self):
-        return 'test delete'
-
-@slash.fixture
-def run_server():
-    butler = ButlerTest()
-    t = threading.Thread(target=butler.run, kwargs={'host': 'localhost', 'port': '8888'})
-    t.daemon = True
-    t.start()
-    time.sleep(1)
-
-    def stop_server():
-        requests.get('http://localhost:8888/stop')
-        time.sleep(1)
-
-    slash.add_cleanup(stop_server)
 
 @slash.parametrize('method', ['get', 'post', 'put', 'delete'])
 def test_methods(run_server, method):
@@ -38,5 +10,3 @@ def test_methods(run_server, method):
     r = getattr(requests, method)('{}/test_{}'.format(base_url, method))
     assert r.content == 'test {}'.format(method)
 
-if __name__ == '__main__':
-    ButlerTest().run(host='localhost', port='8888')
