499349ec3dc9d91cd1f14f4d95a8a507daea6438
Eli Plaut <eli@sentinelone.com>
2016-06-20 08:55:06 +0300

    ButlerFunction: add documentation

--- butler/butler_function.py
+++ butler/butler_function.py
@@ -2,7 +2,14 @@ import inspect
 
 
 class ButlerFunction(object):
+    """ButlerFunction is object to parse Butler's functions' properties"""
+
     def __init__(self, function_name, function_object):
+        """Init properties, using inspect to get function's parameters.
+
+        :param function_name: name of the function.
+        :param function_object: the object of the function.
+        """
         self.function_name = function_name
         function_name_parts = self.function_name.split('_', 1)  # handle function that doesn't contains underscore
         self.method, self.name = function_name_parts[0].upper(), function_name_parts[-1]
@@ -12,6 +19,11 @@ class ButlerFunction(object):
         self.defaults = defaults if defaults else []
 
     def get_urls(self):
+        """Returns all available paths for function.
+        starts with the function name (without leading method, and double underscores changed to slashes
+        and than functions parameters by there order, parameters with default values can be ommitted
+        by their order
+        """
         urls = []
         params = ['<{}>'.format(x) for x in self.args]
         args_length = len(self.args) - len(self.defaults)
@@ -20,10 +32,14 @@ class ButlerFunction(object):
             urls.append(self._get_url(params[:index]))
         return urls
 
-    def get_default(self, key):
+    def get_default(self, name):
+        """Returns default value for argument by `name`
+
+        :param name: the name of the required parameter
+        """
         rargs = [_ for _ in reversed(self.args)]
         rdefaults = [_ for _ in reversed(self.defaults)]
-        return rdefaults[rargs.index(key)]
+        return rdefaults[rargs.index(name)]
 
     def _get_base_url(self):
         """returns the prefix for the view function, double underscore changed to slash"""
