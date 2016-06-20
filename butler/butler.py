c2f2665ec0b91d168a3c3561c5ce3d413bb67b21
Eli Plaut <eli@sentinelone.com>
2016-06-20 08:45:15 +0300

    Butler: add documentation

--- butler/butler.py
+++ butler/butler.py
@@ -6,11 +6,32 @@ from .butler_function import ButlerFunction
 from .client import ButlerClient
 
 class Butler(object):
+    """Butler is ease to use Flask wrapper.
+    It allows to develop and maintain small http server, with no need to handle flask app, 
+    and provide a client which communicate with it.
+    Moreover, using Butler as a app container,allows you to change the data of the object
+    and reload the http server without losing you data.
+
+    To add more views to the Butler object, you need to add a method starting with the method
+    name (supported method are get, post, put and delete), and the view name (double-underscore
+    will interpate to slash
+
+    The following example will add route `/api/get_data`:
+
+        class MyButler(Butler):
+            def get_api__get_data(self):
+            return self.data
+    """
+
     def __init__(self):
+        """init all parameters"""
+        # create Flask application
         self._app = Flask(__name__)
-        self.data = {}
+
+        # register functions to app routes
         self.functions = []
         self._register_urls()
+
         # Flask defaults
         self.protocol = 'http'
         self.host = '127.0.0.1'
@@ -19,9 +40,11 @@ class Butler(object):
 
     @property
     def base_url(self):
+        """Get base url of application"""
         return '{0.protocol}://{0.host}:{0.port}'.format(self)
 
     def _register_urls(self):
+        """Read class functions and register the matching routes"""
         functions = inspect.getmembers(self, predicate=inspect.ismethod)
         for function_name, function_object in functions:
             function = ButlerFunction(function_name, function_object)
@@ -32,7 +55,15 @@ class Butler(object):
                     self._app.add_url_rule(url, methods=[function.method], view_func=function_object)
 
     def run(self, *args, **kwargs):
-        args = list(args)
+        """Start flask application, get all paramters as Flask.run method"""
+        self._update_app_paramters(*args, **kwargs)
+        kwargs.pop('host', None)
+        kwargs.pop('port', None)
+        self._app.run(host=self.host, port=self.port, *args, **kwargs)
+
+    def _update_app_paramters(self, *args, **kwargs):
+        """Parse `run` function parameters and updates `host`, `port` and `protocol` properties"""
+        args = list(args)  # args is tuple, which is immutable
         try:
             self.host = args[0]
             self.port = args[1]
@@ -42,11 +73,13 @@ class Butler(object):
             self.host = kwargs['host']
         if 'port' in kwargs:
             self.port = kwargs['port']
-        if 'ssl_context' in kwargs:
+        if 'ssl_context' in kwargs and kwargs['ssl_context']:
             self.protocol = 'https'
-        self._app.run(*args, **kwargs)
+        else:
+            self.protocol = 'http'
 
     def get_stop(self):
+        """Stop the Flask application"""
         func = request.environ.get('werkzeug.server.shutdown')
         if func is None:
             raise RuntimeError('Not running with the Werkzeug Server')  # pragma: no cover
